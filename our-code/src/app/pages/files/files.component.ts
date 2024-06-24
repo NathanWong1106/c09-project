27092e08e10986311a5fa37828f97d42773d0ea9
Eric <fcweric@gmail.com>
2024-06-24 10:44:25 -0400

    Updated Prisma schema and created new component for files

---
+++ our-code/src/app/pages/files/files.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TreeTableModule } from 'primeng/treetable';
+
+@Component({
+  selector: 'app-files',
+  standalone: true,
+  imports: [],
+  templateUrl: './workspaces.component.html',
+  styleUrl: './workspaces.component.css'
+})
+
+export class WorkspacesComponent {
+
+}
