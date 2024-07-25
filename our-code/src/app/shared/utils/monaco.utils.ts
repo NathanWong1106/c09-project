02c592f8364a32833f3c8631e7063082cad241da
duccie42 <76762011+arwinfong@users.noreply.github.com>
2024-07-24 23:34:14 -0400

    feat: updated comments ui

--- our-code/src/app/shared/utils/monaco.utils.ts
+++ our-code/src/app/shared/utils/monaco.utils.ts
@@ -2,6 +2,7 @@ import uniqolor from 'uniqolor';
 import { createAbsolutePositionFromRelativePosition, createRelativePositionFromTypeIndex } from 'yjs';
 import { Position } from 'monaco-editor';
 import { Doc } from 'yjs';
+import { style } from '@angular/animations';
 
 /**
  * Update or create the style in the head for the given clients.
@@ -42,6 +43,123 @@ export const injectStyleForClients = (clients: number[]) => {
   }
   document.head.appendChild(style);
 };
+
+/**
+ * Update or create the style in the head for the given comments.
+ *
+ */
+export const injectStyleforComments = () => {
+  const style = document.getElementById('monacoCommentStyles') ||
+    document.createElement('style');
+  style.id = 'monacoCommentStyles';
+  const commentStyle = `
+    /* width */
+    ::-webkit-scrollbar {
+      width: 10px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+      background: #1e1e1e; 
+    } 
+    
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      background: #343434; 
+    }
+
+    *,
+    ::after,
+    ::before {
+      /* https://developer.mozilla.org/en-US/docs/Web/CSS/box-sizing */
+      box-sizing: border-box;
+    }
+
+    .comment {
+      border-top: 1px solid #0c74bc;
+      height: 100%;
+      display: flex;
+      width: 90vw;
+      background-color: #252525; 
+      align-items: center; 
+      justify-content: center; 
+    }
+
+    .end {
+      display: flex;
+      justify-content: end;
+    }
+
+    .user-content {
+      height: 100%;
+      display: flex; 
+      flex-direction: column; 
+      justify-content: space-between;
+    }
+
+    .comment-email {
+      color: #bdbdbd;
+      font-weight: bold; 
+    }
+
+    .comment-content {
+      color: #bdbdbd;
+      border: 1px solid #1a4666;
+      background-color: #1e1e1e;
+      padding: 3px;
+      overflow-y: auto; 
+      overflow-x: hidden;
+      margin-right: 5px;
+      height: 50px;
+    }
+
+    .button {
+      font-size: 20px; 
+      margin-top: 5px; 
+      color: white; 
+      border: none; 
+      border-radius: 5px; 
+      cursor: pointer;
+    }
+
+    .button-col {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    .button-row {
+      display: flex; 
+      align-items: center;    
+    }
+
+    .button:hover {
+      opacity: 0.7;
+    }
+
+    .like {
+      background-color: transparent; 
+    }
+
+    .dislike {
+      background-color: transparent; 
+    } 
+
+    .count {
+      margin-right: 15px;
+      color: white;
+    }
+
+    .delete {
+      background-color: transparent;
+    }
+  `;
+
+  style.innerHTML = commentStyle;
+
+  document.head.appendChild(style);
+}
 
 /**
  * create a new relative position from a monaco position
